13fca5fb6f9a5d944aac36f32d33952dc9ca3b9a
junio_vjc@hotmail.com <junio@hotmail.com>
2017-10-01 22:02:49 -0400

    Create project

---
+++ src/main/java/com/pge/pontointeligente/api/PontoIntegenteApplication.java
@@ -0,0 +1,12 @@
+package com.pge.pontointeligente.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoIntegenteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoIntegenteApplication.class, args);
+	}
+}
